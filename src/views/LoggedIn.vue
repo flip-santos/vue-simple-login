b9cc463958199fa5a9c81a196c897c129b5affa0
Flip Santos <flip.b.santos@gmail.com>
2019-11-23 16:01:59 -0300

    Adding login and logged-in pages structure and routes

---
+++ src/views/LoggedIn.vue
@@ -0,0 +1,14 @@
+<template>
+	<v-app id="login-page">
+    <v-content>
+      <v-container class="fill-height" fluid >
+        <h1>Logged In</h1>
+      </v-container>
+    </v-content>
+  </v-app>
+</template>
+<script>
+	export default {
+		name: 'logged-in'
+	}
+</script>
