0f6d41f88a59261b270c425f2e4434e015f89962
Flip Santos <flip.b.santos@gmail.com>
2019-11-25 09:48:48 -0300

    Login form static validation

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -9,12 +9,19 @@
                 <v-toolbar-title>Login form</v-toolbar-title>                
               </v-toolbar>
               <v-card-text>
-                <v-form>
+                
+                <v-form
+                  ref="login_form"
+                  v-model="valid"
+                  :lazy-validation="lazy"
+                >
+                  
                   <v-text-field
-                    label="Login"
-                    name="login"
+                    label="E-mail"
+                    name="email"
                     prepend-icon="person"
                     type="text"
+                    :rules="email_rules"
                   />
 
                   <v-text-field
@@ -23,12 +30,14 @@
                     name="password"
                     prepend-icon="lock"
                     type="password"
+                    :rules="password_rules"
                   />
+
                 </v-form>
               </v-card-text>
               <v-card-actions>
                 <v-spacer />
-                <v-btn color="primary">Login</v-btn>
+                <v-btn color="primary" :disabled="!valid" @click="validate">Log In</v-btn>
               </v-card-actions>
             </v-card>
           </v-col>
@@ -42,6 +51,33 @@
 
 export default {
   name: 'login',
-  components: {}
+  data() {
+    return {
+      valid: true,
+      email: '',
+      email_rules: [
+        v => !!v || 'E-mail is required',
+        v => /.+@.+\..+/.test(v) || 'E-mail must be valid',
+      ],
+      password_rules: [
+        v => !!v || 'Password is required',
+        v => /./.test(v) || 'Password must be valid',
+      ],
+      lazy: false
+    }
+  },
+  methods: {
+    validate () {
+      if (this.$refs.login_form.validate()) {
+        console.log('Entrou!')
+      }
+    },
+    resetForm () {
+      this.$refs.login_form.reset()
+    },
+    resetValidation () {
+      this.$refs.login_form.resetValidation()
+    },
+  }
 }
 </script>
