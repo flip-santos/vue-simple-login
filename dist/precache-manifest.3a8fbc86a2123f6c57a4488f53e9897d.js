c9dd243318d1251f5c658c2411faafc9c1594d4a
Flip Santos <flip.b.santos@gmail.com>
2019-11-26 13:14:43 -0300

    Adding Heroku app deploy

---
+++ dist/precache-manifest.3a8fbc86a2123f6c57a4488f53e9897d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "377afe67fa262a684ba8",
+    "url": "/css/app.523ccf88.css"
+  },
+  {
+    "revision": "54f291e650e9082a85bb",
+    "url": "/css/chunk-vendors.45b8b17c.css"
+  },
+  {
+    "revision": "0cbb1e1623cab9dde5a35895839ff48d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "105cf8c604a7373934f8",
+    "url": "/js/about.09730806.js"
+  },
+  {
+    "revision": "377afe67fa262a684ba8",
+    "url": "/js/app.ea5793d8.js"
+  },
+  {
+    "revision": "54f291e650e9082a85bb",
+    "url": "/js/chunk-vendors.82c00a1d.js"
+  },
+  {
+    "revision": "9d6fbc9e7b57bb6870b63ac8480a7ba3",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
